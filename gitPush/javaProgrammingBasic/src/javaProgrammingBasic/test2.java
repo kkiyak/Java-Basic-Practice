80b8cf1c94fe6f71e26c78457aea833af65103ce
jjy <jjy@DESKTOP-PPMST1M>
2017-02-13 17:21:44 +0900

    test2

---
+++ gitPush/javaProgrammingBasic/src/javaProgrammingBasic/test2.java
@@ -0,0 +1,11 @@
+package javaProgrammingBasic;
+import java.io. *;
+
+public class test2 {
+	public static void main(String[] args) throws IOException
+		{
+		 	System.out.println(123);
+			System.out.println("\\100받았다");
+			System.out.println("내일 또 만나요");
+		}
+}
