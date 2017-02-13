85bc3b153903955498a5bca1a4bb7fe3e5bbbff3
jjy <jjy@DESKTOP-PPMST1M>
2017-02-13 17:28:39 +0900

    .

--- gitPush/javaProgrammingBasic/src/javaProgrammingBasic/test.java
+++ gitPush/javaProgrammingBasic/src/javaProgrammingBasic/test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main(String[] args)
 		{
 		 	System.out.println(123);
-			System.out.println("\\100받았다");
+			System.out.println("\\100 받았다");
 			System.out.println("내일 또 만나요");
 		}
 }
